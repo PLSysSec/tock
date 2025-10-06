3c0c9727ce97b0c322362a27d9839ad579203892
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2025-10-06 09:32:26 -0700

    Fix pointer-width related type errors

--- lean_proofs/LeanProofs/TcbTheoremsTheoremDiv2Pow2.lean
+++ lean_proofs/LeanProofs/TcbTheoremsTheoremDiv2Pow2.lean
@@ -4,5 +4,5 @@ def f_pow2_0 (a0 : Int) : Bool :=
   (let a1 := (BitVec.ofInt 32 a0); ((a0 > 0) && ((BitVec.and a1 (BitVec.sub a1 1#32)) = 0#32)))
 
 -- THEOREM --
-def tcb_theorems_theorem_div2_pow2 : Prop := (∀ (reftgen_n_0 : Int), (∀ (__ : Int), (((f_pow2_0 reftgen_n_0) ∧ (reftgen_n_0 ≥ 2)) -> (∀ (__ : Int), ((reftgen_n_0 ≥ 0) -> (∀ (__ : Int), ((reftgen_n_0 ≤ 18446744073709551615) -> (((reftgen_n_0 / 2) * 2) = reftgen_n_0))))))))
+def tcb_theorems_theorem_div2_pow2 : Prop := (∀ (reftgen_n_0 : Int), (∀ (__ : Int), (((f_pow2_0 reftgen_n_0) ∧ (reftgen_n_0 ≥ 2)) -> (∀ (__ : Int), ((reftgen_n_0 ≥ 0) -> (∀ (__ : Int), ((reftgen_n_0 ≤ 4294967295) -> (((reftgen_n_0 / 2) * 2) = reftgen_n_0))))))))
 
