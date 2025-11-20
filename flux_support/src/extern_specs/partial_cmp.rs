deb8ddb84b5089576dd007ff22301e296ff86fe0
Andrew Cheung <a7cheung@ucsd.edu>
2025-11-19 18:07:05 -0800

    Extern spec for min

--- flux_support/src/extern_specs/partial_cmp.rs
+++ flux_support/src/extern_specs/partial_cmp.rs
@@ -3,7 +3,16 @@
 use core::cmp::PartialOrd;
 
 #[flux_rs::extern_spec(core::cmp)]
+trait Ord {
+    #[flux_rs::no_panic]
+    fn min(self, other: Self) -> Self
+    where
+        Self: Sized;
+}
+
+#[flux_rs::extern_spec(core::cmp)]
 trait PartialOrd<Rhs: ?Sized = Self>: PartialEq<Rhs> {
+    #[flux_rs::no_panic]
     fn lt(&self, other: &Rhs) -> bool;
     fn le(&self, other: &Rhs) -> bool;
 }
