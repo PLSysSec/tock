59404c5b502010f5ded99e9fc7f4a51c741f8f09
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2026-05-01 09:17:54 -0700

    Save old

--- kernel/src/collections/ring_buffer.rs
+++ kernel/src/collections/ring_buffer.rs
@@ -29,14 +29,6 @@ flux_rs::defs! {
     fn full<T>(rb: RingBuffer<T>) -> bool { rb.hd == next_index(rb.tl, ring_len(rb)) }
     fn next_hd<T>(rb: RingBuffer<T>) -> int { next_index(rb.hd, ring_len(rb)) }
     fn next_tl<T>(rb: RingBuffer<T>) -> int { next_index(rb.tl, ring_len(rb)) }
-
-    fn lsslice<T>(s: RingBuffer<T>) -> Slc<T> {
-        if s.hd < s.tl {
-            subslice(s.ring, s.hd, s.tl)
-        } else {
-            subslice(s.ring, s.hd, len(s.ring) - 1)
-        }
-    }
 }
 
 impl<'a, T: Copy> RingBuffer<'a, T> {
@@ -88,10 +80,10 @@ impl<'a, T: Copy> RingBuffer<'a, T> {
     #[flux_rs::spec(fn(&Self[@slf]) ->
         (
             Option<SSlice<T>{ v : 
-                slf.hd < slf.tl => v == subslice(slf, slf.hd, slf.tl) &&
-                slf.hd > slf.tl => v == subslice(slf, slf.hd, len(v) - 1)
+                slf.hd < slf.tl => v == subslice(v, slf.hd, slf.tl) &&
+                slf.hd > slf.tl => v == subslice(v, slf.hd, len(v) - 1)
             }>, 
-            Option<SSlice<T>{ v : slf.hd > slf.tl => v == subslice(slf, 0, slf.tl) }>
+            Option<SSlice<T>{ v : slf.hd > slf.tl => v == subslice(v, 0, slf.tl) }>
         )
     )]
     pub fn as_sslices(&'a self) -> (Option<SSlice<'a, T>>, Option<SSlice<'a, T>>) {
@@ -278,45 +270,6 @@ impl<T: Copy> queue::Queue<T> for RingBuffer<'_, T> {
         }
 
         self.tail = dst;
-    }
-}
-
-mod vec_queue {
-
-    use crate::RingBuffer;
-
-    #[flux_rs::opaque]
-    #[flux_rs::refined_by(elems: Slc<T>)]
-    struct VecQueue<T> {
-        inner: Vec<T>
-    }
-
-    #[flux_rs::trusted]
-    impl<T> VecQueue<T> {
-
-        #[flux_rs::spec(fn(self: &mut Self[@slf], T[@elem])
-            ensures self: Self[push(slf, elem)]
-        )]
-        fn push_back(&mut self, elem: T) {
-            self.inner.push(elem);
-        }
-
-        #[flux_rs::spec(fn(self: &mut Self[@slf], T[@elem])
-            ensures self: Self[pop_front(slf, elem)]
-        )]
-        fn pop_front(&mut self) {
-            if !self.inner.is_empty() {
-                self.inner.remove(0);
-            }
-        }
-    }
-
-    #[flux_rs::spec(fn(r: &mut RingBuffer<T>[@rb], v: &mut VecQueue<T>[@vq], T[@e], T[e]) -> bool[#res]
-        requires 
-    )]
-    fn push_correct<T>(r: &mut RingBuffer<'_, T>, v: &mut VecQueue<T>, e1: T, e2: T) -> bool {
-        v.push_back(e1);
-        r.enqueue(e2)
     }
 }
 
